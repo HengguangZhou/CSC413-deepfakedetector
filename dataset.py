3e475318bb650f2bc56577f62f88797304348b49
Brendan Kan <brendan0110901@gmail.com>
2021-04-05 00:17:38 -0700

    Removed unused imports for dataset class

--- dataset.py
+++ dataset.py
@@ -1,10 +1,6 @@
 import os
 import torch
-import pandas as pd
-from skimage import io, transform
 import numpy as np
-import matplotlib.pyplot as plt
-import torchvision.transforms as T
 from torch.utils.data import Dataset, DataLoader
 from PIL import Image
 import glob
