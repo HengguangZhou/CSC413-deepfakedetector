d0fb52a9d2c4f9c068fc8c5e76129ab104ac1208
jack <qq835490396@gmail.com>
2021-04-19 17:27:18 -0400

    Added in comments

--- dataset.py
+++ dataset.py
@@ -16,6 +16,9 @@ class PairedImagesDataset(Dataset):
     def __init__(self, data_path, size=50000, transform=None, enable_fake_pairs=False):
         """
         :param data_path: Root directory of the image data
+        :param size: size of the total data length
+        :param transform: transform the output data into a specific data structure, default is tensor
+        :param enable_fake_pairs: True if testing for cnn_pairwise, should be left as false for all other experiments
         """
         self.data_path = data_path
         self.size = size
@@ -32,6 +35,15 @@ class PairedImagesDataset(Dataset):
         return self.size
 
     def __getitem__(self, idx):
+        """
+        :param idx: Not used since each pair is sampled randomly
+        The return value are as follows: real image, unknown image, label for first image, label for second image,
+        output label.
+        Output label's value is determined by if user enables enable_fake_pairs, if not enabled, output label takes the
+        value as the same as the label for second image, if enabled, output label outputs 1 if both labels are the same
+        and 0 if both labels are different from each other
+        """
+
         img1, img2, np_label1, np_label2 = self.get_image_pair()
         real_image = self.transform(img1)
         fake_image = self.transform(img2)
@@ -48,6 +60,13 @@ class PairedImagesDataset(Dataset):
         return real_image, fake_image, label1, label2, label
 
     def load_images(self, path, real):
+        """
+        :param path: Root directory of the image data
+        :param real: 1 if the path leads to real images, 0 if the path leads to fake images
+        Load in all files under path and append a label associated with each file.
+        Label is 1 if the image is real, and 0 if the image is fake.
+        Return a list of tuples of the format (image, label)
+        """
         images = []
         label = 0.0
         if real:
@@ -59,6 +78,13 @@ class PairedImagesDataset(Dataset):
         return images
 
     def get_image_pair(self):
+        """
+        Randomly get a pair of real image and an unknown image.
+        Real image is randomly sampled from a list of all real images
+        Unknown image is randomly sampled from a pooled list where
+        the pooled list consist of both real and fake images
+        Return the first and second image, then their respective labels
+        """
         pooled_images = self.real_images + self.fake_images
         img1_info = random.choice(self.real_images)
         if self.enable_fake_pairs:
